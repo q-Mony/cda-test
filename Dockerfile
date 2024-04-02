9d88a0fd202b9e81b71642d13d9a1f50c8352d0b
qm0921 <yang.qingming@tudb.ai>
2024-04-02 17:26:47 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -34,9 +34,5 @@ WORKDIR /opt/serve
 
 # Expose port 5000 (default port for serve)
 EXPOSE 3000
-# 设置默认值为 http://127.0.0.1:8080
-ARG HTTP_URL=http://192.168.31.210:8080
-ENV HTTP_URL $HTTP_URL
-
 # Start serve to serve the application
 CMD ["serve", "-s", "."]
--- src/utils/Static_Util.js
+++ src/utils/Static_Util.js
@@ -1,12 +1,17 @@
-
 // utils.js
-const myEnvVar = process.env.HTTP_URL;
-
-// 打印环境变量的值
-console.log("我的环境变量",myEnvVar);
+const myEnvVar = process.env.NODE_ENV;
+let https_url = "http://192.168.31.210:8080";
+// 判断环境变量的值
+if (myEnvVar === "production") {
+  https_url = "https://api.b18a.xyz";
+} else if (myEnvVar === "development") {
+  console.log("开发模式");
+  https_url = "http://192.168.100.55:32775"
+} else {
+  console.log("未知模式");
+}
 class StaticUtils {
-  
-  static https_url = "http://192.168.31.210:8080";
+  static https_url = https_url;
   // 获取环境变量的值
 }
 
