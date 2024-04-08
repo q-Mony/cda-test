0a0b1ad322efc6d1926cee455639c9a4f3771db5
qm0921 <yang.qingming@tudb.ai>
2024-04-08 15:21:28 +0800

    update

--- src/utils/Static_Util.js
+++ src/utils/Static_Util.js
@@ -1,10 +1,14 @@
 // utils.js
 const https_url = process.env.REACT_APP_API_URL;
 const kya_https_url = process.env.REACT_APP_KYA_API_URL;
-
+const online_https_url = process.env.REACT_APP_ONLINE_API_URL;
+const online_kya_https_url = process.env.REACT_APP_ONLINE_KYA_API_URL;
+const node_mode = process.env.NODE_ENV;
 class StaticUtils {
-  static https_url = https_url;
-  static kya_https_url = kya_https_url;
+  
+  static https_url = node_mode === "production" ? online_https_url : https_url;
+  static kya_https_url = node_mode === "production" ? online_kya_https_url : kya_https_url;
+
   // 获取环境变量的值
 }
 
